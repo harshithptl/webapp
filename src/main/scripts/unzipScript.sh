7d52e337717ff191b7c4d238acac22918dc6904b
Harshith Patil <harshithptl@gmail.com>
2025-02-05 19:36:19 -0500

    Adding unzip installation to script

--- src/main/scripts/unzipScript.sh
+++ src/main/scripts/unzipScript.sh
@@ -3,6 +3,8 @@
 # Update the package lists
 echo "Updating package lists and upgrading packages"
 sudo apt update && sudo apt upgrade -y
+# Install zip unzip
+sudo apt-get install zip unzip
 
 # Install PostgreSQL
 echo "Installing PostgreSQL"
@@ -43,6 +45,14 @@ echo "Setting password for user 'csye6225user'..."
 echo "csye6225user:$PASSWORD" | sudo chpasswd
 
 echo "Successfully set password for user 'csye6225user'"
+
+# Create PostgreSQL user and grant privileges
+#echo "Creating PostgreSQL user and granting privileges..."
+#sudo -u postgres psql -c "CREATE USER csye6225user WITH ENCRYPTED PASSWORD '$PASSWORD';"
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE csye6225db TO csye6225user;"
+#sudo -u postgres psql -c "ALTER ROLE csye6225user WITH LOGIN;"
+#
+#echo "PostgreSQL user 'csye6225user' has been created and granted all privileges on 'csye6225db'"
 
 # Update the permissions
 echo "Setting permissions for /opt/csye6225 directory"
